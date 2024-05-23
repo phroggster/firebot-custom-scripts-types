000c795ee9d334ec5af7438775cd4195c3a1e0f9
phroggster <phroggster@users.noreply.github.com>
2024-05-23 06:09:44 -0500

    Add chatIsConnected getter to TwitchChat

--- types/modules/twitch-chat.d.ts
+++ types/modules/twitch-chat.d.ts
@@ -1,4 +1,16 @@
 export type TwitchChat = {
+    /** Whether or not the streamer is currently connected. */
+    chatIsConnected(): boolean;
+    /**
+     * Sends the message as the bot if available, otherwise as the streamer.
+     * If a whisperTarget is provided, the message will be whispered.
+     * If the message is too long, it will be automatically broken into multiple fragments and sent individually.
+     *
+     * @param message The message to send
+     * @param whisperTarget If provided, message will be whispered to the given user.
+     * @param accountType Which account to chat as. Defaults to bot if available otherwise, the streamer.
+     * @param replyToMessageId A message id to reply to
+     */
     sendChatMessage(
         message: string,
         whisperTarget?: string,
