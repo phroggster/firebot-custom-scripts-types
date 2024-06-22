f48e9d4120aff02fe72716487c1363a0665d7509
phroggster <phroggster@users.noreply.github.com>
2024-06-21 22:30:35 -0500

    Expose twitchChat.once, and add .vs to gitignore
    It's probably worthwhile to add all of the node event syntax, but for
    now, just add .once to twitchChat.

--- types/modules/twitch-chat.d.ts
+++ types/modules/twitch-chat.d.ts
@@ -11,6 +11,15 @@ export type TwitchChat = {
         listener: (...args: ExpectedArgs[]) => ReturnPayload
     );
     /**
+     * Adds a one-time listener function to the end of the listeners array for the event named eventName. The next time eventName is triggered, this listener is removed and then invoked.
+     * @param eventName The name of the event.
+     * @param listener The callback function.
+     */
+    once<ExpectedArgs extends Array<any> = [], ReturnPayload = void>(
+        eventName: string | symbol,
+        listener: (...args: ExpectedArgs[]) => ReturnPayload
+    );
+    /**
      * Sends the message as the bot if available, otherwise as the streamer.
      * If a whisperTarget is provided, the message will be whispered.
      * If the message is too long, it will be automatically broken into multiple fragments and sent individually.
