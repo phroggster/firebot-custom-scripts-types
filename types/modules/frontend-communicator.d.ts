a14ca143cfd9101883122cc6f1a4775ca40e44ab
phroggster <phroggster@users.noreply.github.com>
2025-03-23 09:30:51 -0500

    jsDoc frontendCommunicator
    - Breaking: CEASE DUPLICATING ARRAY RANKINGS
    - Breaking: add fireEventAsync argument type template

--- types/modules/frontend-communicator.d.ts
+++ types/modules/frontend-communicator.d.ts
@@ -1,15 +1,45 @@
+/** A module for facilitating passing events to, and handling events raised by, the frontend. */
 export type FrontendCommunicator = {
-    send(eventName: string, data: unknown): void;
-    on<ExpectedArgs extends Array<any> = [], ReturnPayload = void>(
+    /** Send a synchronous event to the frontend.
+     * @template TEvent The type of event data to supply.
+     * @param eventName The name of the event to send.
+     * @param data Data to provide to the event handler.
+     */
+    send<TEvent = unknown>(
         eventName: string,
-        callback: (...args: ExpectedArgs[]) => ReturnPayload
+        data: TEvent
     ): void;
-    onAsync<ExpectedArgs extends Array<any> = [], ReturnPayload = void>(
+
+    /** Handle a synchronous event triggered by the frontend.
+     * @template TEvent The type of data provided with the event.
+     * @template TResult The type of data returned from the event handler.
+     * @param eventName The name of the event to handle.
+     * @param callback A function to handle the event.
+     */
+    on<TEvent extends Array<unknown> = [], TResult = void>(
         eventName: string,
-        callback: (...args: ExpectedArgs[]) => Promise<ReturnPayload>
+        callback: (...args: TEvent) => TResult
     ): void;
-    fireEventAsync<ReturnPayload = void>(
+
+    /** Handle an asynchronous event triggered by the frontend.
+     * @template TEvent The type of data provided with the event.
+     * @template TResult The type of data returned from the event handler.
+     * @param eventName The name of the event to handle asynchronously.
+     * @param callback A function to asynchronously handle the event.
+     */
+    onAsync<TEvent extends Array<unknown> = [], TResult = void>(
         eventName: string,
-        data: unknown
-    ): Promise<ReturnPayload>;
+        callback: (...args: TEvent) => Promise<TResult>
+    ): void;
+
+    /** Send an asynchronous event to the frontend that is capable of returning a result.
+     * @template TEvent The type of data provided with the event.
+     * @template TResult The type of data returned from the event handler.
+     * @param eventName The name of the event to send.
+     * @param data The data to provide with the event.
+     */
+    fireEventAsync<TEvent = unknown, TResult = void>(
+        eventName: string,
+        data: TEvent
+    ): Promise<TResult>;
 };
