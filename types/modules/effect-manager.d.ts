1e1c4aa02c13233c341e47eb65fe8af5e55388b4
phroggster <phroggster@users.noreply.github.com>
2025-03-27 06:11:48 -0500

    effect-manager: add templating for getEffectById and registerEffect

--- types/modules/effect-manager.d.ts
+++ types/modules/effect-manager.d.ts
@@ -2,8 +2,18 @@ import { Effects } from "../effects";
 import EffectType = Effects.EffectType;
 
 export type EffectManager = {
-    getEffectById: <EffectModel>(
+    getEffectById<
+        EffectModel,
+        OverlayData = unknown,
+        Outputs extends Record<string, unknown> = Record<string, unknown>
+    >(
         effectId: string
-    ) => EffectType<EffectModel> | undefined;
-    registerEffect: <EffectModel>(effectType: EffectType<EffectModel>) => void;
+    ): EffectType<EffectModel, OverlayData, Outputs> | undefined;
+    registerEffect<
+        EffectModel,
+        OverlayData = unknown,
+        Outputs extends Record<string, unknown> = Record<string, unknown>
+    >(
+        effectType: EffectType<EffectModel, OverlayData, Outputs>
+    ): void;
 };
